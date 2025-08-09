81462f67b5a0316d18ab473a559ee7973bf4f84d
Abhayashrestha <153590601+Abhayashrestha@users.noreply.github.com>
2025-08-09 15:11:33 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("----------------------------------------------------------")
+    print("-------------Abhaya's Pokemon Card corner-----------------")
+    print("----------------------------------------------------------")
+
+    option1=int(input("press 1 to view card in stock"))
+    option2=int(input("press 2 to buy card"))
+
+main()
+__name__="main"
+
+
